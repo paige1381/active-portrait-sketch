217efba87f37687f29ae9d293c4611eca3701329
Paige Boyer <paige.boyer@nike.com>
2019-11-06 08:48:35 -0800

    revert back to reading image from local file

--- sketch_1.js
+++ sketch_1.js
@@ -8,11 +8,14 @@ let img;
 let startSize;
 const finalSize = 10;
 
+function preload() {
+    img = loadImage("test-image.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(232, 236, 230);
 
-  img = createImg("https://active-portrait-images.s3-us-west-2.amazonaws.com/test-image.png");
   imageMode(CENTER);
   img.resize(0, height/2);
   
