3cb0fae88faa1b99d09a3a93028a9587d6922179
Paige Boyer <paige.boyer@nike.com>
2019-11-08 15:14:08 -0800

    create server to write remote images locally and serve pages

---
+++ server.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const https = require("https");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/favicon.ico', (req, res) => {})
+
+app.get('/:id', async (request, response) => {
+    const url = `https://active-portrait-images.s3-us-west-2.amazonaws.com/${request.params.id}.png`;
+    const file = fs.createWriteStream("public/temp.png");
+    const req = https.get(url, (res) => {
+        res.pipe(file);
+        res.on('end', () => {
+            response.render('index');
+          });
+    })
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
